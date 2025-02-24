88bf6e9f8a16c81685bd171dea90ad270a8709a0
arthurschlatthaus <arthur.schlatthaus@aol.com>
2025-02-24 18:04:50 +0100

    update cors

--- backend/src/cors.php
+++ backend/src/cors.php
@@ -1,5 +1,13 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
+$allowed_origins = [
+    "http://localhost:3000",
+    "https://puremt2.de",
+    "https://www.puremt2.de"
+];
+
+if (isset($_SERVER["HTTP_ORIGIN"]) && in_array($_SERVER["HTTP_ORIGIN"], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+}
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
