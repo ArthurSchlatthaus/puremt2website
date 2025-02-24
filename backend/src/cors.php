c0f069e67b954f65923e880a3a384ee9369ee67f
arthurschlatthaus <arthur.schlatthaus@aol.com>
2025-02-24 18:08:19 +0100

    update cors

--- backend/src/cors.php
+++ backend/src/cors.php
@@ -7,12 +7,16 @@ $allowed_origins = [
 
 if (isset($_SERVER["HTTP_ORIGIN"]) && in_array($_SERVER["HTTP_ORIGIN"], $allowed_origins)) {
     header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+} else {
+    header("Access-Control-Allow-Origin: https://puremt2.de"); // Default to production
 }
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+// Handle preflight request
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
     http_response_code(200);
     exit();
 }
-?>
