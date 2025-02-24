c67bc374eacb9a4523fd4b6ed4b044cbdb62765f
arthurschlatthaus <arthur.schlatthaus@aol.com>
2025-02-24 20:00:05 +0100

    update cors

--- frontend/src/axios-config.js
+++ frontend/src/axios-config.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080", timeout: 5000,
+    baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080",
+    timeout: 5000,
 });
 
-apiClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => Promise.reject(error));
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default apiClient;
